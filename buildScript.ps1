ac7fe35b2b943687234e5798a3ce68fe1ba16835
dezord <dezord@users.noreply.github.com>
2015-06-14 13:25:54 -0400

    Made build script and added gradle wrapper

---
+++ buildScript.ps1
@@ -0,0 +1,17 @@
+﻿Write-Output "Removing old builds"
+Remove-Item -Recurse .\build\libs\*
+
+Write-Output "Building..."
+.\gradlew build
+
+echo "Updating mcmod.info"
+$name = Get-ChildItem -Name .\build\libs
+
+Move-Item .\build\libs\$name .\build\libs\coherence.zip
+.\7z\7za.exe d .\build\libs\coherence.zip mcmod.info
+.\7z\7za.exe a .\build\libs\coherence.zip mcmod.info
+Move-Item .\build\libs\coherence.zip .\build\libs\$name
+
+Write-Output "Copying to Minecraft dir for testing"
+Remove-Item $env:APPDATA\.minecraft\mods\coherence-*.jar
+Copy-Item .\build\libs\$name $env:APPDATA\.minecraft\mods\
